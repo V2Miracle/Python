8975a63e4cbd807fc3d7ffc1479879c5442d2fd3
v2miracle <v2miracle@163.com>
2018-04-10 20:08:35 +0800

    New file

---
+++ CharExam.py
@@ -0,0 +1,16 @@
+#/usr/bin/env python3
+# -*- conding : utf-8 -*-
+print('I\'m \"ok\"!')
+print('\\\t\\')
+#r''内的代码不转义
+print('r\'\'内的代码不转义:\n')
+print(r'\\\t\\')
+
+print('\'\'\'...\'\'\'可以表示多行内容')
+print('''The first line
+The second line
+The third line''')
+
+print(r'''The first line
+The second line
+The third line''')
---
+++ IOput.py
@@ -0,0 +1,6 @@
+#/usr/bin/env python3
+#-*- coding: utf-8 -*-
+print('Hello Python','on github!')
+print('caculate: 100+200:',100+200)
+name = input('Please input you name:\n')
+print('Your name is :',name)
